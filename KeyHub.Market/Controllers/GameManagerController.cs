53bcc7d5d169600529270af8c81c67716740a057
kamil-kurzaj <kamil.kurzaj04@gmail.com>
2025-09-05 09:15:15 +0200

    Created AddGame partial view

--- KeyHub.Market/Controllers/GameManagerController.cs
+++ KeyHub.Market/Controllers/GameManagerController.cs
@@ -1,6 +1,31 @@
-namespace KeyHub.Market.Controllers;
-
-public class GameManagerController
-{
-    
-}
+// using KeyHub.Market.Models;
+// using Microsoft.AspNetCore.Mvc;
+//
+// namespace KeyHub.Market.Controllers;
+//
+// public class GameManagerController : Controller
+// {
+//
+//
+//     [HttpPost]
+//     public async Task<IActionResult> AddGame(
+//         string title,
+//         Genre genre,
+//         decimal price,
+//         int discount,
+//         Platform platform,
+//         int stock,
+//         IFormFile imageFile)
+//     {
+//         if (string.IsNullOrEmpty(title) || price < 0 || stock < 0 || imageFile == null)
+//         {
+//             ModelState.AddModelError("", "Invalid data");
+//             return View();
+//         }
+//
+//     
+//     
+//     
+//     
+//     
+// }
